84273a3a9d990558f9edd34115de3d0e7bfb96a2
robinsones001 <124541477+robinsones001@users.noreply.github.com>
2023-11-17 12:49:36 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -65,6 +65,10 @@ export const ALL_MODELS = [
     available: ENABLE_GPT4,
   },
   {
+    name: "gpt-4-vision-preview",
+    available: ENABLE_GPT4,
+  },
+  {
     name: "gpt-4-0314",
     available: ENABLE_GPT4,
   },
